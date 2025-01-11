843624fd1e6df22545cb3289db7aea3e173c9ed6
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-11 23:42:46 +0530

    invalid cmd

--- app/main.c
+++ app/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -7,8 +7,15 @@ int main() {
   // Uncomment this block to pass the first stage
   printf("$ ");
 
-  // Wait for user input
+  // Wait for user input and exit when user inputs
   char input[100];
   fgets(input, 100, stdin);
+  
+
+  // remove trailling newline ('\n') as user enter cmds the array of character
+  // by adding null terminator to last index that point last index to '\0'
+  input[strlen(input)-1]='\0';
+  printf("%s: command not found\n",input);
+
   return 0;
 }
