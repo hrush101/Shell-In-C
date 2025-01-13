0908e42b8efc3ad945a6333b274c1dff47a1ec17
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-14 00:03:46 +0530

    cmd path

--- app/main.c
+++ app/main.c
@@ -1,12 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <io.h> // for windows system calls like _access() and X_OK
+
+
+// function to return fully qualified path
+char *get_path(char *cmd){
+
+	// returning pointer pointing to full_path array
+	 char *r_path = NULL;  
+	   
+	// this will return ';' seprated executable dir paths in envirnoment variable 'PATH'
+	char *path = getenv("PATH"); 
+
+	// check if path is null and retun if true
+	if (path == NULL){
+		return NULL;
+	}
+
+	char *path_copy = strdup(path); // create a copy of ENV var PATH as we should not modify orignal path variable
+    
+	// this splits the path_copy string into individual directories using ';' as a delimiter
+	char *dir_path = strtok(path_copy, ";"); 
+
+	char full_path[2000]; // array to store fully qualified path from root ex - /usr/bin/ls
+    
+	// travel tokens / paths seprated by delimiter ';' 
+	while (dir_path != NULL)
+	{
+	  // concatinate dir and cmd to fully qualified path from root 
+	  // ex - C:\\Windows\\System32\\echo.exe
+	  snprintf(full_path , sizeof(full_path) , "%s\\%s.exe" , dir_path , cmd);
+
+	  // Check if C:\\Windows\\System32\\echo.exe exists as executable
+	  if( _access(full_path,X_OK )== 0 ){
+		    r_path=strdup(full_path); // will create a copy of path array and return pointer to caller 
+			free(path_copy);// if call returns from here free allocated path_copy
+		    return r_path;
+	
+		// parameter taken by _access 1st-fully qualified path of cmd bin and 2nd-mode using int - 0,1,2,4 or bitwise OR
+		// 1 or X_OK Checks if the file is executable.
+		// 0 or F_OK Checks if the file exits.
+		// 2 or R_OK Checks if the file is readable.
+        // 4 or W_OK Checks if the file is writable.
+
+	  }
+
+	  dir_path= strtok(NULL,";"); // returns next token/dir path until it becomes null
+	}
+    
+	free(path_copy); // free copied path
+	return NULL; // if it access method return -1 it will not return full_path 
+	// so this will get return eventually to caller
+	
+};
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  while(-1){
+  while(1){
  	 // print promt and flush stdout to print it on screen
  	 printf("$ ");
  	 fflush(stdout);
@@ -36,18 +89,27 @@ int main() {
 
         	       char *cmd = &input[(strlen("type")+1)];
               	       int found = 0;       
-	      	       for(int i=0;i<3;i++){
+	      	        for(int i=0;i<3;i++){
          
         	           if(!strcmp(ptr[i],cmd)){
                 	      printf("%s is a shell builtin\n",cmd);
-		   	      found = 1;
+		   	        found = 1;
 		              break;
 		  	 }
 	       	}
-	       
-	       	if(found != 1){
-            	  printf("%s: not found\n", cmd);                
-	      	 }
+            
+			// If not a shell builtin, search in PATH env variable
+			if(found != 1){
+				char *cmd_path=get_path(cmd);
+
+				if (cmd_path != NULL){
+					printf("%s is %s\n", cmd, cmd_path);
+					free(cmd_path);
+				}
+				else{
+					printf("%s: not found\n", cmd);
+				}
+			}
 	       
 	 }else{
                printf("%s: command not found\n",input);
