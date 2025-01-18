2ea44c73663299a2d2466c76245f25efac17ddc1
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-18 18:43:10 +0530

    pwd final test 1

--- app/main.c
+++ app/main.c
@@ -94,12 +94,7 @@ int main() {
             char *str = &input[(strlen("echo")+1)];
 	       	printf("%s\n",str);
 	    } else if (!strncmp(input,"type",strlen("type"))){
-               	char *ptr[] = {
-                		"pwd",
-         				"echo",
-         				"type",
-         				"exit"
-				};
+               	char *ptr[] = {"pwd","echo","type","exit","which"};
 
         	        char *cmd = &input[(strlen("type")+1)];
               	    int found = 0;       
