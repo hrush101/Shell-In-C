000f5d66eb78f57d38d90a9b07d11bd97f4f63a2
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-22 01:34:54 +0530

    backslash final 3

--- app/main.c
+++ app/main.c
@@ -128,8 +128,8 @@ char* process_echo(char *str) {
             }
         } else if (current == '\\' && in_double_quotes) {
             i++;
-            if (str[i+1] == '\0') { break; } // If backslash is the last character, break
-            if (str[i] == '"' || str[i] == '\\' || str[i] == '$' || str[i] == '\n') {
+            if (str[i] == '\0') { break; } // If backslash is the last character, break
+            if (str[i] == '"' || str[i] == '\\' || str[i] == '$' || str[i] == '\n' || str[i] == '\0') {
                 buffer[buffer_index++] = str[i]; // Append the escaped character
             } 
 			else {
