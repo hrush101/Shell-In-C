3fb877d51daf6b3103d38a68afbe561161a3b2de
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-26 01:20:35 +0530

    \double quotes 4

--- app/main.c
+++ app/main.c
@@ -152,7 +152,7 @@ char* process_echo(char *str) {
 
                 if (str[i] != '\0') {
 
-					if ( str[i + 1] == '\"' || str[i + 1] == '\\' || str[i + 1] == '$' || str[i + 1] == '\n' ) {
+					if ( str[i + 1] == '"' || str[i + 1] == '\\' || str[i + 1] == '$' || str[i + 1] == '\n' ) {
 					    
 						// i++; // Skip the backslash
                     	buffer[buffer_index++] = str[ i + 1 ]; // Append the next character
