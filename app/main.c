59214d21fbcfb75506dea967ccf998d11add9df7
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-01-26 00:35:52 +0530

    double backslash test 6

--- app/main.c
+++ app/main.c
@@ -147,7 +147,7 @@ char* process_echo(char *str) {
 
                 if (str[i] != '\0') {
 
-					if ( (str[i] == '\\' && str[i + 1] == '\"') || (str[i] == '\\' && str[i + 1] == '\\') ) {
+					if ( str[i + 1] == '\"' || str[i + 1] == '\\' ) {
 					    
 						buffer[buffer_index++] = str[i+1];
 
