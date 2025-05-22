449f62d71792081368c20334faa636ddaadcb225
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-05-22 17:21:59 +0530

    redirection to 14

--- app/main.c
+++ app/main.c
@@ -464,7 +464,7 @@ void process_redirection(char *str){
     
 	if (pid == 0) {  
         
-		printf("%s cmd is : ",args[0]);
+		printf("%s cmd is : ",first_cmd);
         fd = open(file_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
         if (fd < 0) {
