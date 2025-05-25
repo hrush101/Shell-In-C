8b7fb148e4821e382795c0a9a3786020ffc5ce0b
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-05-25 19:03:43 +0530

    redirection final 2

--- app/main.c
+++ app/main.c
@@ -445,7 +445,7 @@ void process_redirection(char *str){
 	// here we are extracting 1st string / cmd with arguments and stop till we reach > operator
 	while ( *( str + i ) != operator) {
         
-		if ( *( str + i - 1) == fd_num ) {  // if redirection operator's 1st charecter is file descripter break copy until fd_num
+		if ( *( str - 1) == fd_num ) {  // if redirection operator's 1st charecter is file descripter break copy until fd_num
             
 			break;
 
