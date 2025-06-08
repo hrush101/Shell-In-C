81649b7ff39a127f49b1f96eb0a0155d8b5e2080
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-06-08 17:04:30 +0530

    readline uname

--- your_program.sh
+++ your_program.sh
@@ -2,14 +2,12 @@
 
 set -e # Exit early if any commands fail
 
-
-(
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
   if [[ "$(uname -m)" == "x86_64" ]];then
     export CPPFLAGS="-I/usr/local/opt/readline/include"
     export LDFLAGS="-L/usr/local/opt/readline/lib"
   fi
   gcc app/*.c -o /tmp/shell-target
-)
+
 
 exec /tmp/shell-target "$@"
