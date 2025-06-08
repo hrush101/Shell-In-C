a65c899626eb765a4a941b18c85e8cb3c1de21ca
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-06-08 19:36:54 +0530

    dummy 2

--- your_program.sh
+++ your_program.sh
@@ -3,10 +3,10 @@
 set -e # Exit early if any commands fail
 
 cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-sudo apt-get install libreadline-dev
-export CPPFLAGS="-I/usr/include/readline"
-export LDFLAGS="-L/usr/lib/x86_64-linux-gnu"
+sudo dnf install readline-devel
+export CPPFLAGS="-I/usr/include"
+export LDFLAGS="-L/usr/lib64"
 
-gcc $CPPFLAGS $LDFLAGS app/*.c -o /tmp/shell-target
+gcc $CPPFLAGS $LDFLAGS -lreadline app/*.c -o /tmp/shell-target
 
 exec /tmp/shell-target "$@"
