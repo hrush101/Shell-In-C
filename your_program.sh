70704da1a408468985f73abc5724fd39b29afd03
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-06-08 19:24:06 +0530

    readline uname 6

--- your_program.sh
+++ your_program.sh
@@ -5,34 +5,20 @@ echo "Remote architecture: $(ssh $remote_host uname -m)"
 
 cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
 
-if [ "$(uname -m)" = "arm64" ]; then
-  # Apple Silicon macOS
-  export CPPFLAGS="-I/opt/homebrew/Cellar/readline/8.2.13/include"
-  export LDFLAGS="-L/opt/homebrew/Cellar/readline/8.2.13/lib"
-elif [ "$(uname -m)" = "x86_64" ]; then
+
+
+if [ "$(uname -m)" = "x86_64" ]; then
+   
+  if ! brew list readline >/dev/null 2>&1; then
+    brew install readline
+  fi
+
   # Intel macOS (Homebrew default prefix)
   export CPPFLAGS="-I/usr/local/opt/readline/include"
   export LDFLAGS="-L/usr/local/opt/readline/lib"
 fi
 
-#!/bin/sh
+gcc $CPPFLAGS $LDFLAGS app/*.c -o /tmp/shell-target
 
-set -e # Exit early if any commands fail
-echo "Remote architecture: $(ssh $remote_host uname -m)"
 
-cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-
-if [ "$(uname -m)" = "arm64" ]; then
-  # Apple Silicon macOS
-  export CPPFLAGS="-I/opt/homebrew/Cellar/readline/8.2.13/include"
-  export LDFLAGS="-L/opt/homebrew/Cellar/readline/8.2.13/lib"
-elif [ "$(uname -m)" = "x86_64" ]; then
-  # Intel macOS (Homebrew default prefix)
-  export CPPFLAGS="-I/usr/local/opt/readline/include"
-  export LDFLAGS="-L/usr/local/opt/readline/lib"
-fi
-
-cmake -B build -S .
-cmake --build build
-
-exec ./build/shell "$@"
+exec /tmp/shell-target "$@"
