14a78a4acf5353d255c68d02efee648b556ddde4
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-06-08 17:14:58 +0530

    readline uname 2

--- your_program.sh
+++ your_program.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-if [[ "$(uname -m)" == "arm64" ]]; then
+if [[ "$(uname -m)" == "x86_64" ]]; then
   export CPPFLAGS="-I/opt/homebrew/Cellar/readline/8.2.13/include"
   export LDFLAGS="-L/opt/homebrew/Cellar/readline/8.2.13/lib"
 fi
