bb4505667d8948b18b1198491c5a90a36e99c9fa
hrushikesh ingale <hrushikeshingale6@gmail.com>
2025-06-08 19:20:08 +0530

    readline uname 5

--- your_program.sh
+++ your_program.sh
@@ -15,7 +15,24 @@ elif [ "$(uname -m)" = "x86_64" ]; then
   export LDFLAGS="-L/usr/local/opt/readline/lib"
 fi
 
-gcc $CPPFLAGS $LDFLAGS app/*.c -o /tmp/shell-target
+#!/bin/sh
 
+set -e # Exit early if any commands fail
+echo "Remote architecture: $(ssh $remote_host uname -m)"
 
-exec /tmp/shell-target "$@"
+cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+
+if [ "$(uname -m)" = "arm64" ]; then
+  # Apple Silicon macOS
+  export CPPFLAGS="-I/opt/homebrew/Cellar/readline/8.2.13/include"
+  export LDFLAGS="-L/opt/homebrew/Cellar/readline/8.2.13/lib"
+elif [ "$(uname -m)" = "x86_64" ]; then
+  # Intel macOS (Homebrew default prefix)
+  export CPPFLAGS="-I/usr/local/opt/readline/include"
+  export LDFLAGS="-L/usr/local/opt/readline/lib"
+fi
+
+cmake -B build -S .
+cmake --build build
+
+exec ./build/shell "$@"
